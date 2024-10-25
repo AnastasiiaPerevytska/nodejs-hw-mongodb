04be70e49ed0c9723a09693a8fa4309f273d3c58
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-10-25 20:12:52 +0300

    get status added

--- src/server.js
+++ src/server.js
@@ -25,9 +25,17 @@ export const setupServer = () => {
   });
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-    console.log(contacts);
+    if (contacts.length === 0) {
+      res.status(404).json({
+        status: 404,
+        message: 'Contacts not found',
+        error: 'Looks like the database of contacts is empty',
+      });
+      return;
+    }
 
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -39,24 +47,31 @@ export const setupServer = () => {
 
     if (!contact) {
       res.status(404).json({
+        status: 404,
         message: 'Contact not found',
+        error: `The requested contact with id ${contactId} was not found`,
       });
       return;
     }
 
     res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   });
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
+      status: 404,
       message: 'Not found',
+      error: `The requested resource ${req.url} was not found`,
     });
   });
 
   app.use((err, req, res, next) => {
     res.status(500).json({
+      status: 500,
       message: 'Something went wrong',
       error: err.message,
     });
