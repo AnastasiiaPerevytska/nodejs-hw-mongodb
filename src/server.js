cf511d4375f559e19fd5a96288651df2a923e321
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-11-17 20:40:05 +0200

    hw5 test

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
+
 
 const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
@@ -12,6 +15,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -26,7 +30,8 @@ export const setupServer = () => {
     });
   });
 
-  app.use(contactsRouter);
+  app.use('/contacts', contactsRouter);
+  app.use('/auth', authRouter);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
