36dc9b828ce464267557f9ec43ecc2dacef95328
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-11-26 00:45:21 +0200

    createcontacts service changes

--- src/server.js
+++ src/server.js
@@ -30,8 +30,9 @@ export const setupServer = () => {
     });
   });
 
-  app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
+  app.use('/contacts', contactsRouter);
+
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
